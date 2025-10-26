0f7dba69655a7e6e75399687c18646fa8aaa5346
Debottam <debottammandal445@gmail.com>
2025-10-26 10:35:49 +0530

    changes

--- src/features/auth/components/logout-form.tsx
+++ src/features/auth/components/logout-form.tsx
@@ -2,12 +2,48 @@
 
 import { Button } from "@/components/ui/button"
 import { authClient } from "@/lib/auth-client"
+import { Loader } from "lucide-react"
+import { useRouter } from "next/navigation"
+import { useState } from "react"
+import { toast } from "sonner"
 
 export const LogoutForm = () => {
+    const router = useRouter();
+    const [isPending, setIsPending] = useState(false);
+
+    const handleLogout = async () => {
+        setIsPending(true);
+        try {
+            await authClient.signOut({
+                fetchOptions: {
+                onSuccess: () => {
+                    router.push("/login");
+                    router.refresh();
+                },
+                },
+            });
+        } 
+        catch (error) {
+            const err = error as Error
+            console.error("Logout failed:", err);
+            toast.error(err?.message || "Something went wrong while logging out");
+        }
+        finally{
+            setIsPending(false);
+        }
+    }
+
     return (
         <div>
-            <Button onClick={() => authClient.signOut()}>
-                Logout
+            <Button
+                type="submit"
+                disabled={isPending}
+                onClick={handleLogout}
+            >
+                {isPending && (
+                    <Loader className="h-4 w-4 animate-spin" />
+                )}
+                {isPending ? "Logging out ..." : "Logout"}
             </Button>
         </div>
     )

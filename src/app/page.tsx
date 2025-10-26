43b83b97d88032b251f911b5700bc2c5525c7ec8
Debottam <debottammandal445@gmail.com>
2025-10-27 01:54:40 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,29 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
 import { LogoutForm } from "@/features/auth/components/logout-form";
-import { requireAuth } from "@/lib/auth-utils";
-import { caller } from "@/trpc/server";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
 
-const Page = async () => {
-  await requireAuth();
-
-  const data = await caller.getUsers();
+const Page = () => {
+  const trpc = useTRPC()
+  const queryClient = useQueryClient();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
+  const create = useMutation(trpc.createWorkflow.mutationOptions({
+    onSuccess: () => {
+      toast.success("Job queued");
+    }
+  }));
 
   return (
     <div className="flex flex-col gap-y-4 px-40 items-center justify-center min-h-screen">
-      {JSON.stringify(data)}
+      <div>
+        {JSON.stringify(data)}
+      </div>
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>
+        Create Workflow
+      </Button>
       <LogoutForm />
     </div>
   );
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,13 +1,20 @@
+import { inngest } from '@/inngest/client';
 import { createTRPCRouter, protectedProcedure } from '../init';
 import db from '@/lib/db';
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(({ ctx }) => {
-    return db.user.findMany({
-      where: {
-        id: ctx.auth.user.id,
+  getWorkflows: protectedProcedure.query(() => {
+    return db.workflow.findMany();
+  }),
+  createWorkflow: protectedProcedure.mutation(async () => {
+    await inngest.send({
+      name: "test/hello.world",
+      data: {
+        email: "debmsn43@gmail.com"
       }
     })
-  }),
+
+    return { success: true, message: "job queued" }
+  })
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
